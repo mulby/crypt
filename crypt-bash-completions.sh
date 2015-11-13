6c1ab544e79eca724df0b0cb08b27a09013663ea
Gabe Mulley <gabe@edx.org>
2015-11-13 08:16:56 -0500

    Tweak completion behavior

--- crypt-bash-completions.sh
+++ crypt-bash-completions.sh
@@ -1,12 +1,11 @@
 #!bash
 
 _crypt() {
-    COMPREPLY=()
     local cur prev words cword
     _get_comp_words_by_ref -n : cur prev words cword
     case "$prev" in
         crypt)
-            COMPREPLY=( $(compgen -W "-h --help init add cat clip ls rm mv exec pwgen edit" -- $cur) )
+            COMPREPLY=( $(compgen -W "init add cat clip ls rm mv exec pwgen edit" -- $cur) )
             ;;
         add|clip|cat|rm|mv|edit)
             local keys=$(crypt ls)
@@ -14,7 +13,6 @@ _crypt() {
             ;;
         *)
             local keys=$(ls)
-            COMPREPLY=( $(compgen -W "$keys" -- $cur) )
             ;;
     esac
 }
